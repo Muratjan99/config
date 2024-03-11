a761bce920c734a344fb13ba354bc9083d44da22
Muratjan99 <61548531+Muratjan99@users.noreply.github.com>
2024-03-11 14:48:22 +0800

    Update yuedong.js

--- scripts/yuedong.js
+++ scripts/yuedong.js
@@ -6,17 +6,14 @@
 脚本作者：MRT
 更新时间：2023-10-22
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-*******************************/
-/**
- * @fileoverview Example of HTTP rewrite of request header.
- *
- * @supported Quantumult X (v1.0.5-build188)
- *
- * [rewrite_local]
- * ^https://club.lirenos.com url script-request-header test.js
- */
+*******************************
+[rewrite_local]
+^https://club.lirenos.com url script-request-header test.js
 
-// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+[mitm] 
+hostname = club.lirenos.com
+*******************************/
+$request.scheme, $request.method, $request.url, $request.path, $request.headers
 
 var modifiedHeaders = $request.headers;
 modifiedHeaders['Cookie'] = 'JSESSIONID=D5B0E672C15FE1FF365CA8B8DBCFB16A; Path=/; Secure; HttpOnly';
