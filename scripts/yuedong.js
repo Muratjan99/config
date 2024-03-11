fe8613ae199a03a1b0b9041824d08b3e814a6143
Muratjan99 <61548531+Muratjan99@users.noreply.github.com>
2024-03-11 14:42:18 +0800

    Update yuedong.js

--- scripts/yuedong.js
+++ scripts/yuedong.js
@@ -6,14 +6,21 @@
 脚本作者：MRT
 更新时间：2023-10-22
 使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-*******************************
-[rewrite_local]
-^https://club.lirenos.com url script-response-body https://raw.githubusercontent.com/Muratjan99/configs/main/ltpn.js
-
-[mitm] 
-hostname = club.lirenos.com
 *******************************/
+/**
+ * @fileoverview Example of HTTP rewrite of request header.
+ *
+ * @supported Quantumult X (v1.0.5-build188)
+ *
+ * [rewrite_local]
+ * ^https://club.lirenos.com url script-request-header test.js
+ */
 
-var ret = JSON.parse($response.body);
-ret.data.canPlay = true
-$done({body : JSON.stringify(ret)});
+// $request.scheme, $request.method, $request.url, $request.path, $request.headers
+
+var modifiedHeaders = $request.headers;
+modifiedHeaders['Cookie'] = 'JSESSIONID=D5B0E672C15FE1FF365CA8B8DBCFB16A; Path=/; Secure; HttpOnly';
+
+$done({path: modifiedPath, headers : modifiedHeaders});
+// $done({path : modifiedPath});
+// $done({}); // Not changed.
